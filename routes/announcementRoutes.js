69f5afabb60e936f18b5d9e94120aba60c3a5399
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-01-30 00:20:44 +0530

    Update announcementRoutes.js

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const studentController = require("../controller/announcementController");
+const announcementController = require('../controller/announcementController');
 
-app.get('/sendMail', studentController.sendMail)
+app.get('/sendMail', studentController.sendMail);
+
+app.post('/sendMailById', announcementController.sendEmailByRegNum);
+
+app.get('/sendMailFirstYear', announcementController.firstYearMail);
+
+app.get('/sendMailSecondYear', announcementController.secondYearMail);
 
 module.exports = app;
