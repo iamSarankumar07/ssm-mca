7555104fcb3431cd18aca146e6d685d40ab13d6e
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-01-31 01:07:19 +0530

    Update viewController.js

--- controller/viewController.js
+++ controller/viewController.js
@@ -1,12 +1,39 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const Contact = require("../models/contactModel");
 const Student = require("../models/studentModel");
 const studentFee = require("../models/feesModel");
 const Image = require("../models/imageModel");
-const { errorMonitor } = require("nodemailer/lib/xoauth2");
 const app = express();
 app.use(express.static(path.join(__dirname, "../Images")));
+
+app.set("view engine", "hbs");
+const viewPath = path.join(__dirname, "../view");
+app.set("views", viewPath);
+
+exports.dashboard = async (req, res) => {
+  res.render("admin");
+};
+
+exports.register = async (req, res) => {
+  res.render("register");
+};
+
+exports.announcement = async (req, res) => {
+  res.render("announcement");
+};
+
+exports.firstYearAnnouncement = async (req, res) => {
+  res.render("firstYearAnnouncement");
+};
+
+exports.secondYearAnnouncement = async (req, res) => {
+  res.render("secondYearAnnouncement");
+};
+
+exports.feeRegister = async (req, res) => {
+  res.render("feeRegister");
+};
 
 exports.message = async (req, res) => {
   try {
@@ -19,17 +46,21 @@ exports.message = async (req, res) => {
 };
 
 exports.studentList = async (req, res) => {
-    try {
-      const allStudents = await Student.find({ isDelete: false });
-      const firstYearStudents = allStudents.filter(student => student.year === 1);
-      const secondYearStudents = allStudents.filter(student => student.year === 2);
-  
-      res.render("studentList", { firstYearStudents, secondYearStudents });
-    } catch (err) {
-      console.error(err);
-      res.send("Error", err);
-    }
-  };
+  try {
+    const allStudents = await Student.find({ isDelete: false });
+    const firstYearStudents = allStudents.filter(
+      (student) => student.year === 1
+    );
+    const secondYearStudents = allStudents.filter(
+      (student) => student.year === 2
+    );
+
+    res.render("studentList", { firstYearStudents, secondYearStudents });
+  } catch (err) {
+    console.error(err);
+    res.send("Error", err);
+  }
+};
 
 // exports.studentList = async (req, res) => {
 //   try {
@@ -76,8 +107,12 @@ exports.studentEdit = async (req, res) => {
 exports.studentFeeList = async (req, res) => {
   try {
     const allStudents = await studentFee.find({ isDelete: false });
-    const firstYearStudents = allStudents.filter(student => student.year === 1);
-    const secondYearStudents = allStudents.filter(student => student.year === 2);
+    const firstYearStudents = allStudents.filter(
+      (student) => student.year === 1
+    );
+    const secondYearStudents = allStudents.filter(
+      (student) => student.year === 2
+    );
 
     res.render("studentFeeList", { firstYearStudents, secondYearStudents });
   } catch (err) {
@@ -97,13 +132,13 @@ exports.feeEdit = async (req, res) => {
   }
 };
 
-exports.gallery = async (req, res)=>{
+exports.gallery = async (req, res) => {
   try {
-    const images = await Image.find({})
+    const images = await Image.find({});
     res.render("gallery", { images });
   } catch (err) {
-    console.log(err)
-    res.send(err.message)
+    console.log(err);
+    res.send(err.message);
   }
 };
 
