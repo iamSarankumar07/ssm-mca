acf018a3fb6840bd6303af65b8e0b6e0f82d47f7
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-01-31 01:05:28 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const authFile = require('../middleware/auth')
 const studentController = require("../controller/studentController");
 
-app.post("/studentRegister", studentController.newStudent);
+app.post("/studentRegister", authFile.validateToken, studentController.newStudent);
 
-app.post("/studentUpdate/:userId", studentController.updateStudent);
+app.post("/studentUpdate/:userId", authFile.validateToken, studentController.updateStudent);
 
-app.get("/studentDelete/:userId", studentController.deleteStudent);
+app.get("/studentDelete/:userId", authFile.validateToken, studentController.deleteStudent);
 
 module.exports = app;
