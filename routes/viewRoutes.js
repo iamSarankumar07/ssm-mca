66053663c5f83665558d3df76a2a4b95ab7ae84d
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-01-31 01:05:10 +0530

    Update viewRoutes.js

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,55 +1,61 @@
 const express = require("express");
 const path = require("path");
 const viewController = require("../controller/viewController");
+const authFile = require("../middleware/auth");
 const app = express();
 
 app.set("view engine", "hbs");
 const viewPath = path.join(__dirname, "../view");
 app.set("views", viewPath);
 
+app.use(express.static("images"));
+
 app.use(express.static(path.join(__dirname, "../Images")));
 
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-app.get("/main", (req, res) => {
-  res.render("main");
-});
-app.get("/home", (req, res) => {
-  res.render("home");
-});
-app.get("/announcement", (req, res) => {
-  res.render("announcement");
-});
-app.get("/firstYearAnnouncement", (req, res) => {
-  res.render("firstYearAnnouncement");
-});
-app.get("/secondYearAnnouncement", (req, res) => {
-  res.render("secondYearAnnouncement");
-});
-app.get("/messages", viewController.message);
+app.get("/register", authFile.validateToken, viewController.register);
 
-app.get("/studentList", viewController.studentList);
+app.get("/announcement", authFile.validateToken, viewController.announcement);
 
-app.get("/studentEdit/:userId", viewController.studentEdit);
+app.get(
+  "/firstYearAnnouncement",
+  authFile.validateToken,
+  viewController.firstYearAnnouncement
+);
+
+app.get(
+  "/secondYearAnnouncement",
+  authFile.validateToken,
+  viewController.secondYearAnnouncement
+);
+
+app.get("/messages", authFile.validateToken, viewController.message);
+
+app.get("/studentList", authFile.validateToken, viewController.studentList);
+
+app.get(
+  "/studentEdit/:userId",
+  authFile.validateToken,
+  viewController.studentEdit
+);
 
 // app.get('/studentList/firstYear', viewController.firstYearStudentList);
 
 // app.get('/studentList/secondYear', viewController.secondYearStudentList);
 
-app.get("/upload", (req, res)=>{
-    res.render("imageUpload")
+app.get("/upload", (req, res) => {
+  res.render("imageUpload");
 });
-app.get("/dashboard", (req, res)=>{
-    res.render("admin")
-});
-app.get("/feeRegister", (req, res) => {
-  res.render("feeRegister");
-});
-app.get("/feeList", viewController.studentFeeList )
+app.get("/dashboard", authFile.validateToken, viewController.dashboard);
 
-app.get("/feeEdit/:userId", viewController.feeEdit);
+app.get("/feeRegister", authFile.validateToken, viewController.feeRegister);
 
-app.get("/gallery", viewController.gallery);
+app.get("/sessionExpired", (req, res) => {
+  res.render("sessionExpired");
+});
+app.get("/feeList", authFile.validateToken, viewController.studentFeeList);
+
+app.get("/feeEdit/:userId", authFile.validateToken, viewController.feeEdit);
+
+app.get("/gallery", authFile.validateToken, viewController.gallery);
 
 module.exports = app;
