63722914c89a1ad0836161fc26d4055b2a325818
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-01-31 01:06:27 +0530

    Update feeRoutes.js

--- routes/feeRoutes.js
+++ routes/feeRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const authFile = require('../middleware/auth')
 const app = express();
+
 const feeController = require("../controller/feeController");
 
-app.post("/feeRegister", feeController.newFee);
+app.post("/feeRegister", authFile.validateToken, feeController.newFee);
 
-app.post("/updateFee/:userId", feeController.updateFee);
+app.post("/updateFee/:userId", authFile.validateToken, feeController.updateFee);
 
-app.get("/deleteFee/:userId", feeController.deleteFee);
+app.get("/deleteFee/:userId", authFile.validateToken, feeController.deleteFee);
 
 module.exports = app;
