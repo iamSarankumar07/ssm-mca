e5f69960591e0f23823d21f14e9c1df250e04d1a
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-01-30 12:30:52 +0530

    Update announcementController.js

--- controller/announcementController.js
+++ controller/announcementController.js
@@ -188,6 +188,7 @@ exports.sendEmailByRegNum = async (req, res) => {
 exports.firstYearMail = async (req, res) => {
   try {
     // first-year students
+    const { title, subject, message } = req.body;
     const firstYearStudents = await Student.find({ year: 1 }, "email");
 
     if (!firstYearStudents.length) {
@@ -208,60 +209,72 @@ exports.firstYearMail = async (req, res) => {
 
     const mailOptions = {
       from: "verifyuserofficial@gmail.com",
-      subject: "Happy New Year 🎉.",
+      subject: subject,
     };
 
     for (let i = 0; i < studentEmails.length; i++) {
       mailOptions.to = studentEmails[i];
       mailOptions.html = `
-          <!DOCTYPE html>
-          <html lang="en">
-          <head>
-          <title>Welcome to Our Community!</title>
-          <style>
-          body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-          }
-          .container {
-            max-width: 600px;
-            margin: 20px auto;
-            padding: 20px;
-            background-color: #ffffff;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-          }
-          h1 {
-            color: #ff9900;
-            text-align: center;
-          }
-          p {
-            margin-bottom: 15px;
-          }
-          .footer {
-            margin-top: 20px;
-            font-size: 12px;
-            color: #777777;
-            text-align: center;
-          }
-          </style>
-          </head>
-          <body>
-          <div class="container">
-          <h1>HAPPY NEW YEAR 2024 🎉</h1>
-          <p>Dear Students,</p>
-          <p>As we step into the new year, we want to extend our warmest wishes to you. May the year 2024 bring you joy, success, and prosperity in all your endeavors.<br><br> Have a Great Day 😊...</p>
-          <p>If you have any questions or need further assistance, please feel free to <a href="mailto:verifyuserofficial@gmail.com" style="color: #007bff; text-decoration: none;">contact us</a>.</p>
-          <p>Best regards,<br>Saran Kumar.</p>
-          <div class="footer">
-          This is an automated message. Please do not reply to this email.
-          </div>
-          </div>
-          </body>
-          </html>
-          `;
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <title>${title}</title>
+        <style>/* Font and text styling */
+        body {
+          font-family: 'Poppins', 'Arial', sans-serif;
+          line-height: 1.6;
+          color: #1a1a1a; /* Dark text color */
+          font-size: 16px;
+          background-color: #f4f4f4; /* Light gray background */
+          margin: 0; /* Remove default body margin */
+        }
+    
+        h1 {
+          font-size: 28px; /* Larger heading size */
+          margin-bottom: 15px;
+          color: #ff9900; /* Orange heading color */
+          text-align: center;
+        }
+    
+        p {
+          margin-bottom: 15px;
+          color: #1a1a1a; /* Dark text color */
+        }
+    
+        /* Structure and layout */
+        .container {
+          max-width: 600px;
+          margin: 20px auto;
+          background-color: #ffffff; /* White background */
+          padding: 30px;
+          border-radius: 8px; /* Rounded corners */
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Light shadow for depth */
+        }
+    
+        /* Footer section styling */
+        .footer {
+          margin-top: 20px;
+          font-size: 12px;
+          color: #777777; /* Lighter text color in the footer */
+          text-align: center;
+        }</style>
+        </head>
+        <body>
+        <div class="container">
+        <p>Dear Students,</p>
+        <h1>${title}</h1>
+    <p>${message}</p> 
+    <p>Have a Great Day 😊...</p>
+    <p>If you have any questions or need further assistance, please feel free to <a href="mailto:verifyuserofficial@gmail.com" style="color: #007bff; text-decoration: none;">contact us</a>.</p>
+    <p>Best regards,<br>Saran Kumar.</p>
+    <div class="footer">
+      This is an automated message. Please do not reply to this email.
+    </div>
+  </div>
+</body>
+        </html>
+      `;
+
       transporter.sendMail(mailOptions, (err, info) => {
         if (err) {
           console.log(`Error sending email to ${studentEmails[i]}:`, err);
@@ -271,7 +284,7 @@ exports.firstYearMail = async (req, res) => {
       });
     }
 
-    res.redirect('/ssm/mca/dashboard');
+    res.redirect("/ssm/mca/firstYearAnnouncement");
   } catch (err) {
     res.status(500).json({ message: err.message });
     console.error("Error:", err.message);
@@ -281,15 +294,16 @@ exports.firstYearMail = async (req, res) => {
 exports.secondYearMail = async (req, res) => {
   try {
     // second-year students
+    const { title, subject, message } = req.body;
     const firstYearStudents = await Student.find({ year: 2 }, "email");
 
     if (!firstYearStudents.length) {
-      console.log("No first-year students found.");
+      console.log("No second-year students found.");
       return res.send({ message: "No Second Year Students Found." });
     }
 
     const studentEmails = firstYearStudents.map((user) => user.email);
-    console.log("First-year student emails:", studentEmails);
+    console.log("Second-year student emails:", studentEmails);
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
@@ -301,60 +315,72 @@ exports.secondYearMail = async (req, res) => {
 
     const mailOptions = {
       from: "verifyuserofficial@gmail.com",
-      subject: "Happy New Year 🎉.",
+      subject: subject,
     };
 
     for (let i = 0; i < studentEmails.length; i++) {
       mailOptions.to = studentEmails[i];
       mailOptions.html = `
-          <!DOCTYPE html>
-          <html lang="en">
-          <head>
-          <title>Welcome to Our Community!</title>
-          <style>
-          body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-          }
-          .container {
-            max-width: 600px;
-            margin: 20px auto;
-            padding: 20px;
-            background-color: #ffffff;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-          }
-          h1 {
-            color: #ff9900;
-            text-align: center;
-          }
-          p {
-            margin-bottom: 15px;
-          }
-          .footer {
-            margin-top: 20px;
-            font-size: 12px;
-            color: #777777;
-            text-align: center;
-          }
-          </style>
-          </head>
-          <body>
-          <div class="container">
-          <h1>HAPPY NEW YEAR 2024 🎉</h1>
-          <p>Dear Students,</p>
-          <p>As we step into the new year, we want to extend our warmest wishes to you. May the year 2024 bring you joy, success, and prosperity in all your endeavors.<br><br> Have a Great Day 😊...</p>
-          <p>If you have any questions or need further assistance, please feel free to <a href="mailto:verifyuserofficial@gmail.com" style="color: #007bff; text-decoration: none;">contact us</a>.</p>
-          <p>Best regards,<br>Saran Kumar.</p>
-          <div class="footer">
-          This is an automated message. Please do not reply to this email.
-          </div>
-          </div>
-          </body>
-          </html>
-          `;
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <title>${title}</title>
+        <style>/* Font and text styling */
+        body {
+          font-family: 'Poppins', 'Arial', sans-serif;
+          line-height: 1.6;
+          color: #1a1a1a; /* Dark text color */
+          font-size: 16px;
+          background-color: #f4f4f4; /* Light gray background */
+          margin: 0; /* Remove default body margin */
+        }
+    
+        h1 {
+          font-size: 28px; /* Larger heading size */
+          margin-bottom: 15px;
+          color: #ff9900; /* Orange heading color */
+          text-align: center;
+        }
+    
+        p {
+          margin-bottom: 15px;
+          color: #1a1a1a; /* Dark text color */
+        }
+    
+        /* Structure and layout */
+        .container {
+          max-width: 600px;
+          margin: 20px auto;
+          background-color: #ffffff; /* White background */
+          padding: 30px;
+          border-radius: 8px; /* Rounded corners */
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Light shadow for depth */
+        }
+    
+        /* Footer section styling */
+        .footer {
+          margin-top: 20px;
+          font-size: 12px;
+          color: #777777; /* Lighter text color in the footer */
+          text-align: center;
+        }</style>
+        </head>
+        <body>
+        <div class="container">
+        <p>Dear Students,</p>
+        <h1>${title}</h1>
+    <p>${message}</p> 
+    <p>Have a Great Day 😊...</p>
+    <p>If you have any questions or need further assistance, please feel free to <a href="mailto:verifyuserofficial@gmail.com" style="color: #007bff; text-decoration: none;">contact us</a>.</p>
+    <p>Best regards,<br>Saran Kumar.</p>
+    <div class="footer">
+      This is an automated message. Please do not reply to this email.
+    </div>
+  </div>
+</body>
+        </html>
+      `;
+
       transporter.sendMail(mailOptions, (err, info) => {
         if (err) {
           console.log(`Error sending email to ${studentEmails[i]}:`, err);
@@ -364,13 +390,11 @@ exports.secondYearMail = async (req, res) => {
       });
     }
 
-    res.redirect('/ssm/mca/dashboard');
+    res.redirect("/ssm/mca/secondYearAnnouncement");
   } catch (err) {
     res.status(500).json({ message: err.message });
     console.error("Error:", err.message);
   }
 };
-
-
 
 module.exports = exports;
