259be8a9367b887dcc717f872cffc597149f4e93
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-03-09 03:27:14 +0530

    Update studentController.js

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const Student = require("../models/studentModel");
+const authFile = require("../middleware/auth");
+const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
 const app = express();
 
 exports.newStudent = async (req, res) => {
@@ -8,19 +11,162 @@ exports.newStudent = async (req, res) => {
     name: body.name,
     registerNumber: body.registerNumber,
     gender: body.gender,
-    age: body.age,
+    dob: body.dob,
     year: body.year,
     phone: body.phone,
-    email: body.email
+    email: body.email,
+    totalFee: body.totalFee,
+    pendingFee: body.pendingFee,
+    paymentStatus: body.paymentStatus
   });
+
+  const studentId = student.registerNumber
+  student.studentId = studentId;
+
+  const password = student.dob.toString();
+  console.log(
+    `Name : ${student.name} \nUsername: ${studentId} \nPassword : ${password}`
+  );
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  student.password = hashedPassword;
+
   try {
-    const savedUser = await student.save();
-    console.log("User Added to DataBase...");
+    const a1 = await student.save();
+    console.log("Student Added to DataBase...");
+
+    const transPorter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "verifyuserofficial@gmail.com",
+        pass: "wsdv megz vecp wzen",
+      },
+    });
+
+    const mailOptions = {
+      from: "verifyuserofficial@gmail.com",
+      to: student.email,
+      subject: "Registration Successful.",
+      html: `
+    <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <title>Welcome to Our Community!</title>
+    <style>
+      body {
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size: 16px;
+        line-height: 1.6;
+        color: #333;
+        margin: 0;
+        padding: 0;
+      }
+      .container {
+        max-width: 600px;
+        margin: 20px auto;
+        padding: 20px;
+        background-color: #fff;
+        border-radius: 10px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+      }
+      h1 {
+        font-size: 36px;
+        color: #007bff; 
+        margin-bottom: 20px;
+        text-align: center;
+        text-transform: uppercase;
+      }
+      p {
+        margin-bottom: 15px;
+        text-align: justify;
+      }
+      ul {
+        margin-bottom: 15px;
+        padding-left: 20px;
+      }
+      li {
+        margin-bottom: 5px;
+      }
+      a {
+        color: #007bff;
+        text-decoration: none;
+      }
+      a:hover {
+        text-decoration: underline;
+      }
+      .footer {
+        font-size: 14px;
+        color: #999;
+        margin-top: 20px;
+        text-align: center;
+      }
+      .highlight {
+        background-color: #eaf6ff;
+        padding: 5px 10px;
+        border-radius: 5px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Welcome to SSM College Of Engineering! 🎉</h1>
+      <p>Dear ${student.name},</p>
+      <p>We are thrilled to welcome you to our College! Your account has been successfully Registered 🚀.</p>
+      <p>Here are your login details:</p>
+      <ul>
+        <li><strong>Username : </strong> ${studentId}</li>
+        <li><strong>Password : </strong> ${password}</li>
+      </ul>
+      <p>If you have any questions or need further assistance, please feel free to <a href="mailto:verifyuserofficial@gmail.com" style="color: #007bff; text-decoration: none;">contact us</a>.</p>
+      <p>Best regards,<br>Saran Kumar.</p>
+      <div class="footer">
+        This is an automated message. Please do not reply to this email.
+      </div>
+    </div>
+  </body>
+  </html>
+  `,
+    };
+
+    transPorter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log(err, "Email Sent Failed...");
+      } else {
+        console.log("Email Sent Successfully....");
+      }
+    });
+
     res.redirect("/ssm/mca/register");
-    console.log(savedUser);
   } catch (err) {
-    console.log(err.message);
-    res.send(err.message);
+    res.send(err.message, "Error");
+  }
+};
+
+exports.login = async (req, res) => {
+  const { studentId, password } = req.body;
+
+  try {
+    const student = await Student.findOne({ studentId });
+
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+    const passwordMatch = await bcrypt.compare(password, student.password);
+    if (!passwordMatch) {
+      return res.send(
+        '<script>alert("Wrong Password!"); window.location.href = "/ssm/mca/studentLogin";</script>'
+      );
+    }
+    const accessToken = await authFile.sToken(student);
+    res.cookie("access-token", accessToken, {
+      maxAge: 60 * 60 * 1000,
+    });
+
+    res.redirect("/studentHome");
+  } catch (err) {
+    console.error("Error logging in:", err);
+    res.status(500).send("Failed to login");
   }
 };
 
@@ -41,12 +187,15 @@ exports.updateStudent = async (req, res) => {
     const userId = req.params.userId;
 
     await Student.findByIdAndUpdate(userId, {
-        name: body.name,
-        gender: body.gender,
-        age: body.age,
-        year: body.year,
-        phone: body.phone,
-        email: body.email
+      name: body.name,
+      gender: body.gender,
+      dob: body.dob,
+      year: body.year,
+      phone: body.phone,
+      email: body.email,
+      totalFee: body.totalFee,
+      pendingFee: body.pendingFee,
+      paymentStatus: body.paymentStatus
     });
 
     res.redirect("/ssm/mca/studentList");
