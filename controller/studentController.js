7861e1a2638f2ff0c3a002e35c4fbf6b9da5deb2
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-03-09 17:46:48 +0530

    Update studentController.js

--- controller/studentController.js
+++ controller/studentController.js
@@ -150,7 +150,9 @@ exports.login = async (req, res) => {
     const student = await Student.findOne({ studentId });
 
     if (!student) {
-      return res.status(404).json({ message: "Student not found" });
+      return res.send(
+        '<script>alert("Student not Found"); window.location.href = "/ssm/mca/studentLogin";</script>'
+      );
     }
     const passwordMatch = await bcrypt.compare(password, student.password);
     if (!passwordMatch) {
