f50fb3f4822a35dd57d96a820144351739c37005
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-03-10 12:03:15 +0530

    Update announcementController.js

--- controller/announcementController.js
+++ controller/announcementController.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const Student = require("../models/studentModel");
-const app = express();
 
 exports.sendMail = async (req, res) => {
   try {
@@ -185,19 +184,30 @@ exports.sendEmailByRegNum = async (req, res) => {
   }
 };
 
-exports.firstYearMail = async (req, res) => {
+exports.commonMail = async (req, res) => {
   try {
-    // first-year students
-    const { title, subject, message } = req.body;
-    const firstYearStudents = await Student.find({ year: 1 }, "email");
-
-    if (!firstYearStudents.length) {
-      console.log("No first-year students found.");
-      return res.send({ message: "No First Year Students Found." });
+    const { title, subject, message, year } = req.body;
+    let students = [];
+    
+    if (year === "first") {
+      students = await Student.find({ year: 1 }, "email");
+    } else if (year === "second") {
+      students = await Student.find({ year: 2 }, "email");
+    } else if (year === "all") {
+      students = await Student.find({}, "email");
+    } else {
+      return res.status(400).json({ message: "Invalid year selected." });
     }
 
-    const studentEmails = firstYearStudents.map((user) => user.email);
-    console.log("First-year student emails:", studentEmails);
+    if (!students.length) {
+      console.log("No students found.");
+      return res.send(
+        '<script>alert("Wrong Password!"); window.location.href = "/ssm/mca/commonAnnouncement";</script>'
+      );
+    }
+
+    const studentEmails = students.map((user) => user.email);
+    console.log("Student emails:", studentEmails);
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
@@ -284,113 +294,7 @@ exports.firstYearMail = async (req, res) => {
       });
     }
 
-    res.redirect("/ssm/mca/firstYearAnnouncement");
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-    console.error("Error:", err.message);
-  }
-};
-
-exports.secondYearMail = async (req, res) => {
-  try {
-    // second-year students
-    const { title, subject, message } = req.body;
-    const firstYearStudents = await Student.find({ year: 2 }, "email");
-
-    if (!firstYearStudents.length) {
-      console.log("No second-year students found.");
-      return res.send({ message: "No Second Year Students Found." });
-    }
-
-    const studentEmails = firstYearStudents.map((user) => user.email);
-    console.log("Second-year student emails:", studentEmails);
-
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "verifyuserofficial@gmail.com",
-        pass: "wsdv megz vecp wzen",
-      },
-    });
-
-    const mailOptions = {
-      from: "verifyuserofficial@gmail.com",
-      subject: subject,
-    };
-
-    for (let i = 0; i < studentEmails.length; i++) {
-      mailOptions.to = studentEmails[i];
-      mailOptions.html = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-        <title>${title}</title>
-        <style>/* Font and text styling */
-        body {
-          font-family: 'Poppins', 'Arial', sans-serif;
-          line-height: 1.6;
-          color: #1a1a1a; /* Dark text color */
-          font-size: 16px;
-          background-color: #f4f4f4; /* Light gray background */
-          margin: 0; /* Remove default body margin */
-        }
-    
-        h1 {
-          font-size: 28px; /* Larger heading size */
-          margin-bottom: 15px;
-          color: #ff9900; /* Orange heading color */
-          text-align: center;
-        }
-    
-        p {
-          margin-bottom: 15px;
-          color: #1a1a1a; /* Dark text color */
-        }
-    
-        /* Structure and layout */
-        .container {
-          max-width: 600px;
-          margin: 20px auto;
-          background-color: #ffffff; /* White background */
-          padding: 30px;
-          border-radius: 8px; /* Rounded corners */
-          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Light shadow for depth */
-        }
-    
-        /* Footer section styling */
-        .footer {
-          margin-top: 20px;
-          font-size: 12px;
-          color: #777777; /* Lighter text color in the footer */
-          text-align: center;
-        }</style>
-        </head>
-        <body>
-        <div class="container">
-        <p>Dear Students,</p>
-        <h1>${title}</h1>
-    <p>${message}</p> 
-    <p>Have a Great Day 😊...</p>
-    <p>If you have any questions or need further assistance, please feel free to <a href="mailto:verifyuserofficial@gmail.com" style="color: #007bff; text-decoration: none;">contact us</a>.</p>
-    <p>Best regards,<br>Saran Kumar.</p>
-    <div class="footer">
-      This is an automated message. Please do not reply to this email.
-    </div>
-  </div>
-</body>
-        </html>
-      `;
-
-      transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-          console.log(`Error sending email to ${studentEmails[i]}:`, err);
-        } else {
-          console.log(`Email sent successfully to ${studentEmails[i]}.`);
-        }
-      });
-    }
-
-    res.redirect("/ssm/mca/secondYearAnnouncement");
+    res.redirect("/ssm/mca/commonAnnouncement");
   } catch (err) {
     res.status(500).json({ message: err.message });
     console.error("Error:", err.message);
