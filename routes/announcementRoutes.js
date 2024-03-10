dc2a11194faf551e12ebdb4522c0f6e296d23485
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-03-10 19:28:53 +0530

    Update announcementRoutes.js

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -10,6 +10,6 @@ app.post("/sendMailById", authFile.validateToken, announcementController.sendEma
 
 app.post("/commonAnnouncement", authFile.validateToken, announcementController.commonMail);
 
-app.post("/commonAnnouncement", authFile.validateToken, announcementController.sendPaymentAlert);
+app.post("/paymentAlert", authFile.validateToken, announcementController.sendPaymentAlert);
 
 module.exports = app;
