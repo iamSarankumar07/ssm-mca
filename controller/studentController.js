37bf1423311856cfc2258c1c3ee151d9f21255dd
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-03-16 23:39:47 +0530

    Update studentController.js

--- controller/studentController.js
+++ controller/studentController.js
@@ -7,10 +7,16 @@ const app = express();
 
 exports.newStudent = async (req, res) => {
   const body = req.body;
-  const existingStudent = await Student.findOne({ registerNumber: body.registerNumber });
-    if (existingStudent) {
-        return res.send('<script>alert("Registration number already exists!"); window.location="/ssm/mca/register";</script>');
-    }
+
+  const existingStudent = await Student.findOne({
+    registerNumber: body.registerNumber,
+  });
+  if (existingStudent) {
+    return res.send(
+      '<script>alert("Registration number already exists!"); window.location="/ssm/mca/register";</script>'
+    );
+  }
+
   const student = new Student({
     name: body.name,
     registerNumber: body.registerNumber,
@@ -156,8 +162,111 @@ exports.newStudent = async (req, res) => {
     });
 
     // res.redirect("/ssm/mca/register");
-    res.send('<script>alert("Registered Successfully!"); window.location="/ssm/mca/register";</script>');
-
+    res.send(
+     `<!DOCTYPE html>
+     <html lang="en">
+     <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Registration</title>
+       <style>
+         body {
+           font-family: Arial, sans-serif;
+           margin: 0;
+           padding: 0;
+           background-color: #f4f4f4;
+         }
+     
+         .modal {
+           display: flex;
+           align-items: center;
+           justify-content: center;
+           position: fixed;
+           z-index: 1000;
+           left: 0;
+           top: 0;
+           width: 100%;
+           height: 100%;
+           overflow: auto;
+           background-color: rgba(0, 0, 0, 0.6);
+         }
+     
+         .modal-content {
+           background-color: #fefefe;
+           padding: 20px;
+           border: 1px solid #ccc;
+           border-radius: 10px;
+           width: 80%;
+           max-width: 400px;
+           box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+         }
+     
+         p {
+           margin: 0 0 20px;
+           font-size: 18px;
+           font-weight: bold;
+           color: #28a745; /* Green color */
+           text-align: center;
+         }
+     
+         .button-container {
+           display: flex;
+           justify-content: center;
+           width: 100%;
+         }
+     
+         button[type="button"] {
+           background-color: #3d6ef5ff;
+           color: #f2f2f2;
+           font-weight: bold;
+           padding: 8px 14px;
+           border: none;
+           font-size: 13px;
+           border-radius: 5px;
+           cursor: pointer;
+           transition: background-color 0.3s;
+         }
+     
+         button[type="button"]:hover {
+           background-color: #f2f2f2;
+           color: #3d6ef5ff;
+           font-weight: bold;
+         }
+       </style>
+     </head>
+     <body>
+       <div id="myModal" class="modal">
+         <div class="modal-content">
+           <p>Registration Successful!</p>
+           <div class="button-container">
+             <button type="button" onclick="redirect()">Continue</button>
+           </div>
+         </div>
+       </div>
+     
+       <script>
+         function redirect() {
+           window.location.href = "/ssm/mca/register";
+         }
+     
+         window.onload = function() {
+           var modal = document.getElementById("myModal");
+     
+           modal.style.display = "flex";
+     
+           window.onclick = function(event) {
+             if (event.target == modal) {
+               modal.style.display = "none";
+               redirect();
+             }
+           }
+         }
+       </script>
+     </body>
+     </html>
+     
+     `
+    );
   } catch (err) {
     res.send(err.message, "Error");
   }
@@ -224,7 +333,111 @@ exports.updateStudent = async (req, res) => {
       examPaymentStatus: body.examPaymentStatus,
     });
 
-    res.redirect("/ssm/mca/studentList");
+    // res.redirect("/ssm/mca/studentList");
+    res.send(
+      `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Registration</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+          }
+      
+          .modal {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: fixed;
+            z-index: 1000;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0, 0, 0, 0.6);
+          }
+      
+          .modal-content {
+            background-color: #fefefe;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            width: 80%;
+            max-width: 400px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+          }
+      
+          p {
+            margin: 0 0 20px;
+            font-size: 18px;
+            font-weight: bold;
+            color: #28a745; /* Green color */
+            text-align: center;
+          }
+      
+          .button-container {
+            display: flex;
+            justify-content: center;
+            width: 100%;
+          }
+      
+          button[type="button"] {
+            background-color: #3d6ef5ff;
+            color: #f2f2f2;
+            font-weight: bold;
+            padding: 8px 14px;
+            border: none;
+            font-size: 13px;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+          }
+      
+          button[type="button"]:hover {
+            background-color: #f2f2f2;
+            color: #3d6ef5ff;
+            font-weight: bold;
+          }
+        </style>
+      </head>
+      <body>
+        <div id="myModal" class="modal">
+          <div class="modal-content">
+            <p>Student Details Updated Successfully!</p>
+            <div class="button-container">
+              <button type="button" onclick="redirect()">Continue</button>
+            </div>
+          </div>
+        </div>
+      
+        <script>
+          function redirect() {
+            window.location.href = "/ssm/mca/studentList";
+          }
+      
+          window.onload = function() {
+            var modal = document.getElementById("myModal");
+      
+            modal.style.display = "flex";
+      
+            window.onclick = function(event) {
+              if (event.target == modal) {
+                modal.style.display = "none";
+                redirect();
+              }
+            }
+          }
+        </script>
+      </body>
+      </html>
+      `
+     );
   } catch (err) {
     console.error(err);
     res.send("Error");
