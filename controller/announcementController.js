e71791bad2a5dfc60e2346f1828b65f5d602bbae
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-03-16 23:40:26 +0530

    Update announcementController.js

--- controller/announcementController.js
+++ controller/announcementController.js
@@ -179,7 +179,111 @@ exports.sendEmailByRegNum = async (req, res) => {
     });
 
     console.log("Email sent successfully");
-    res.redirect("/ssm/mca/announcement");
+    // res.redirect("/ssm/mca/announcement");
+    res.send(
+      `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Registration</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+          }
+      
+          .modal {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: fixed;
+            z-index: 1000;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0, 0, 0, 0.6);
+          }
+      
+          .modal-content {
+            background-color: #fefefe;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            width: 80%;
+            max-width: 400px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+          }
+      
+          p {
+            margin: 0 0 20px;
+            font-size: 18px;
+            font-weight: bold;
+            color: #28a745; /* Green color */
+            text-align: center;
+          }
+      
+          .button-container {
+            display: flex;
+            justify-content: center;
+            width: 100%;
+          }
+      
+          button[type="button"] {
+            background-color: #3d6ef5ff;
+            color: #f2f2f2;
+            font-weight: bold;
+            padding: 8px 14px;
+            border: none;
+            font-size: 13px;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+          }
+      
+          button[type="button"]:hover {
+            background-color: #f2f2f2;
+            color: #3d6ef5ff;
+            font-weight: bold;
+          }
+        </style>
+      </head>
+      <body>
+        <div id="myModal" class="modal">
+          <div class="modal-content">
+            <p>Announcement Sent Succesfully</p>
+            <div class="button-container">
+              <button type="button" onclick="redirect()">Continue</button>
+            </div>
+          </div>
+        </div>
+      
+        <script>
+          function redirect() {
+            window.location.href = "/ssm/mca/announcement";
+          }
+      
+          window.onload = function() {
+            var modal = document.getElementById("myModal");
+      
+            modal.style.display = "flex";
+      
+            window.onclick = function(event) {
+              if (event.target == modal) {
+                modal.style.display = "none";
+                redirect();
+              }
+            }
+          }
+        </script>
+      </body>
+      </html>
+      `
+     );
   } catch (error) {
     console.error("Error sending email:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -298,7 +402,111 @@ exports.commonMail = async (req, res) => {
       });
     }
 
-    res.redirect("/ssm/mca/commonAnnouncement");
+    // res.redirect("/ssm/mca/commonAnnouncement");
+    res.send(
+      `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Registration</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+          }
+      
+          .modal {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: fixed;
+            z-index: 1000;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0, 0, 0, 0.6);
+          }
+      
+          .modal-content {
+            background-color: #fefefe;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            width: 80%;
+            max-width: 400px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+          }
+      
+          p {
+            margin: 0 0 20px;
+            font-size: 18px;
+            font-weight: bold;
+            color: #28a745; /* Green color */
+            text-align: center;
+          }
+      
+          .button-container {
+            display: flex;
+            justify-content: center;
+            width: 100%;
+          }
+      
+          button[type="button"] {
+            background-color: #3d6ef5ff;
+            color: #f2f2f2;
+            font-weight: bold;
+            padding: 8px 14px;
+            border: none;
+            font-size: 13px;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+          }
+      
+          button[type="button"]:hover {
+            background-color: #f2f2f2;
+            color: #3d6ef5ff;
+            font-weight: bold;
+          }
+        </style>
+      </head>
+      <body>
+        <div id="myModal" class="modal">
+          <div class="modal-content">
+            <p>Announcement Sent Succesfully</p>
+            <div class="button-container">
+              <button type="button" onclick="redirect()">Continue</button>
+            </div>
+          </div>
+        </div>
+      
+        <script>
+          function redirect() {
+            window.location.href = "/ssm/mca/commonAnnouncement";
+          }
+      
+          window.onload = function() {
+            var modal = document.getElementById("myModal");
+      
+            modal.style.display = "flex";
+      
+            window.onclick = function(event) {
+              if (event.target == modal) {
+                modal.style.display = "none";
+                redirect();
+              }
+            }
+          }
+        </script>
+      </body>
+      </html>
+      `
+     );
   } catch (err) {
     res.status(500).json({ message: err.message });
     console.error("Error:", err.message);
@@ -576,7 +784,111 @@ exports.sendPaymentAlert = async (req, res) => {
       }, i * 1000); 
     }
     //  res.send('<script>alert(`Alert Sent to ${year} Year ${type} Fees Pending Students`); window.location.href = "/ssm/mca/paymentAlert";</script>');
-    res.redirect('/ssm/mca/paymentAlert')
+    // res.redirect('/ssm/mca/paymentAlert')
+    res.send(
+      `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Registration</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+          }
+      
+          .modal {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: fixed;
+            z-index: 1000;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0, 0, 0, 0.6);
+          }
+      
+          .modal-content {
+            background-color: #fefefe;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            width: 80%;
+            max-width: 400px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+          }
+      
+          p {
+            margin: 0 0 20px;
+            font-size: 18px;
+            font-weight: bold;
+            color: #28a745; /* Green color */
+            text-align: center;
+          }
+      
+          .button-container {
+            display: flex;
+            justify-content: center;
+            width: 100%;
+          }
+      
+          button[type="button"] {
+            background-color: #3d6ef5ff;
+            color: #f2f2f2;
+            font-weight: bold;
+            padding: 8px 14px;
+            border: none;
+            font-size: 13px;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+          }
+      
+          button[type="button"]:hover {
+            background-color: #f2f2f2;
+            color: #3d6ef5ff;
+            font-weight: bold;
+          }
+        </style>
+      </head>
+      <body>
+        <div id="myModal" class="modal">
+          <div class="modal-content">
+            <p>Payment Alert Sent Succesfully</p>
+            <div class="button-container">
+              <button type="button" onclick="redirect()">Continue</button>
+            </div>
+          </div>
+        </div>
+      
+        <script>
+          function redirect() {
+            window.location.href = "/ssm/mca/paymentAlert";
+          }
+      
+          window.onload = function() {
+            var modal = document.getElementById("myModal");
+      
+            modal.style.display = "flex";
+      
+            window.onclick = function(event) {
+              if (event.target == modal) {
+                modal.style.display = "none";
+                redirect();
+              }
+            }
+          }
+        </script>
+      </body>
+      </html>
+      `
+     );
   } catch (err) {
     return res.send(
       '<script>alert("Error in Payment Alert"); window.location.href = "/ssm/mca/paymentAlert";</script>'
