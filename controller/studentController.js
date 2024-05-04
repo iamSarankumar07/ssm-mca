e019e02f55b6b94f93c168cbf1169d762cfd8ebe
Sarankumar <155176224+iamgrim07@users.noreply.github.com>
2024-05-04 06:05:22 +0530

    Update studentController.js

--- controller/studentController.js
+++ controller/studentController.js
@@ -452,4 +452,269 @@ exports.updateStudent = async (req, res) => {
   }
 };
 
+exports.requestChange = async (req, res) => {
+  const { newName, newEmail, registerNumber, newDob, newPhone } = req.body;
+
+  try {
+      let student = await Student.findOneAndUpdate(
+          { registerNumber: registerNumber },
+          {
+              $set: {
+                  editRequest: {
+                      newName,
+                      newPhone,
+                      newEmail,
+                      newDob,
+                      status: 'requested'
+                  }
+              }
+          },
+          { new: true }
+      );
+
+      res.send(
+        `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Registration</title>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              margin: 0;
+              padding: 0;
+              background-color: #f4f4f4;
+            }
+        
+            .modal {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              position: fixed;
+              z-index: 1000;
+              left: 0;
+              top: 0;
+              width: 100%;
+              height: 100%;
+              overflow: auto;
+              background-color: rgba(0, 0, 0, 0.6);
+            }
+        
+            .modal-content {
+              background-color: #fefefe;
+              padding: 20px;
+              border: 1px solid #ccc;
+              border-radius: 10px;
+              width: 80%;
+              max-width: 400px;
+              box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            }
+        
+            p {
+              margin: 0 0 20px;
+              font-size: 18px;
+              font-weight: bold;
+              color: #28a745; /* Green color */
+              text-align: center;
+            }
+        
+            .button-container {
+              display: flex;
+              justify-content: center;
+              width: 100%;
+            }
+        
+            button[type="button"] {
+              background-color: #3d6ef5ff;
+              color: #f2f2f2;
+              font-weight: bold;
+              padding: 8px 14px;
+              border: none;
+              font-size: 13px;
+              border-radius: 5px;
+              cursor: pointer;
+              transition: background-color 0.3s;
+            }
+        
+            button[type="button"]:hover {
+              background-color: #f2f2f2;
+              color: #3d6ef5ff;
+              font-weight: bold;
+            }
+          </style>
+        </head>
+        <body>
+          <div id="myModal" class="modal">
+            <div class="modal-content">
+              <p>Profile Update Request Sent</p>
+              <div class="button-container">
+                <button type="button" onclick="redirect()">Continue</button>
+              </div>
+            </div>
+          </div>
+        
+          <script>
+            function redirect() {
+              window.location.href = "/ssm/mca/studentProfile";
+            }
+        
+            window.onload = function() {
+              var modal = document.getElementById("myModal");
+        
+              modal.style.display = "flex";
+        
+              window.onclick = function(event) {
+                if (event.target == modal) {
+                  modal.style.display = "none";
+                  redirect();
+                }
+              }
+            }
+          </script>
+        </body>
+        </html>
+        `
+       );
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error in Profile Update Request' });
+  }
+};
+
+exports.approveAndReject = async (req, res) => {
+  let data = req.body;
+  let registerNumber = data.registerNumber
+  let action = data.action;
+  let status;
+  let statusRes;
+  let student;
+
+  if (action === "Approve") {
+    status = "Approved"
+  } else if (action === "Reject") {
+    status = "Rejected"
+  }
+
+  try {
+    if (status === "Approved"){
+      student = await Student.findOne({registerNumber: registerNumber});
+
+      if (student.editRequest && student.editRequest.status === 'requested') {
+          student.name = student.editRequest.newName;
+          student.phone = student.editRequest.newPhone;
+          student.email = student.editRequest.newEmail;
+          student.dob = student.editRequest.newDob;
+          student.editRequest = null;
+          await student.save();
+        };
+        statusRes = "Approved";
+    } else if (status === "Rejected"){
+      student = await Student.findOne({registerNumber: registerNumber});
+
+      if (student.editRequest && student.editRequest.status === 'requested') {
+          student.editRequest = null;
+          await student.save();
+        }
+        statusRes = "Rejected"
+    }
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'verifyuserofficial@gmail.com',
+        pass: 'wsdv megz vecp wzen',
+      },
+    });
+
+    const mailOptions = {
+      from: 'verifyuserofficial@gmail.com',
+      to: student.email,
+      subject: 'Profile Update Request',
+      html: `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <title>Profile Update Request Status</title>
+          <style>
+              body {
+                  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                  font-size: 16px;
+                  line-height: 1.6;
+                  color: #333;
+                  margin: 0;
+                  padding: 0;
+              }
+              .container {
+                  max-width: 600px;
+                  margin: 20px auto;
+                  padding: 20px;
+                  background-color: #fff;
+                  border-radius: 10px;
+                  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+              }
+              h1 {
+                  font-size: 36px;
+                  color: #007bff; 
+                  margin-bottom: 20px;
+                  text-align: center;
+                  text-transform: uppercase;
+              }
+              p {
+                  margin-bottom: 15px;
+                  text-align: justify;
+              }
+              .status {
+                  margin-bottom: 15px;
+                  text-align: center;
+              }
+              a {
+                  color: #007bff;
+                  text-decoration: none;
+              }
+              a:hover {
+                  text-decoration: underline;
+              }
+              .footer {
+                  font-size: 14px;
+                  color: #999;
+                  margin-top: 20px;
+                  text-align: center;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="container">
+              <h1>Profile Update Request 📝</h1>
+              <div class="status">
+                  <p>Dear ${student.name},</p>
+                  <p>Your profile update request has been ${statusRes}.</p>
+                  <p>If you have any questions or need further assistance, please feel free to <a href="mailto:saran@outlook.in" style="color: #007bff; text-decoration: none;">contact us</a>.</p>
+                  <p>Best regards,<br>Sarankumar</p>
+              </div>
+              <div class="footer">
+                  This is an automated message. Please do not reply to this email.
+              </div>
+          </div>
+      </body>
+      </html>
+      `
+  };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log(err, 'Email Sent Failed...');
+      } else {
+        console.log('Email Sent Successfully....');
+      }
+    });
+
+    return res.redirect('/ssm/mca/reviewRequest')
+
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 module.exports = exports;
