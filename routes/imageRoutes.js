1ca9c5f9ca1ed2598706ee1b0a10a68fe08abe11
iamsarankumar7 <iamssarankumar@gmail.com>
2024-12-08 11:17:15 +0530

    dev

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -6,10 +6,12 @@ const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-app.post("/uploadImage", upload.single("image"), imageController.uploadImage);
+app.post("/uploadImageFireBase", upload.single("image"), imageController.uploadImageFireBase);
 app.get("/getImageFromFireBase", imageController.getImagesFromFireBase);
 app.post("/uploadImageCloudinary", upload.single("image"), imageController.uploadImageCloudinary);
 app.get('/getAllImagesFromCloudinary', imageController.getAllImagesFromCloudinary);
+app.post("/uploadImageSupabase", upload.single("image"), imageController.uploadImageSupabase);
+// app.post("/getAllImagesFromSupabase", imageController.getAllImagesFromSupabase);
 app.get("/getImageFromDB", imageController.getImagesFromDB);
 
 module.exports = app;
